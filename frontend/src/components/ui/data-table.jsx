1a1b681e5d1fb441cd7a76c876f07ec2c48cd455
aumpatel2624 <aumpatelc36@gmail.com>
2026-02-11 18:56:03 +0530

    Pagination Error

--- frontend/src/components/ui/data-table.jsx
+++ frontend/src/components/ui/data-table.jsx
@@ -32,6 +32,10 @@ export function DataTable({
     const [columnVisibility, setColumnVisibility] = React.useState({});
     const [columnFilters, setColumnFilters] = React.useState([]);
     const [sorting, setSorting] = React.useState([]);
+    const [pagination, setPagination] = React.useState({
+        pageIndex: 0,
+        pageSize: 10,
+    });
 
     const table = useReactTable({
         data,
@@ -41,12 +45,14 @@ export function DataTable({
             columnVisibility,
             rowSelection,
             columnFilters,
+            pagination,
         },
         enableRowSelection: true,
         onRowSelectionChange: setRowSelection,
         onSortingChange: setSorting,
         onColumnFiltersChange: setColumnFilters,
         onColumnVisibilityChange: setColumnVisibility,
+        onPaginationChange: setPagination,
         getCoreRowModel: getCoreRowModel(),
         getFilteredRowModel: getFilteredRowModel(),
         getPaginationRowModel: getPaginationRowModel(),
